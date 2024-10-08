d0cab0502088eb3ae55560ce38cbe168315159a5
mntsilva <mntsilva@extworten.pt>
2024-10-08 09:54:28 +0100

    first commit

---
+++ dbtlearn/snapshots/scd_raw_listings.sql
@@ -0,0 +1,14 @@
+-- define qual a estrategia de um snapshot (logica de scd2)
+
+{% snapshot scd_raw_listings %}
+{{
+ config(
+ target_schema='DEV',
+ unique_key='id',
+ strategy='timestamp',
+ updated_at='updated_at',
+ invalidate_hard_deletes=True
+ )
+}}
+select * FROM {{ source('airbnb', 'listings') }}
+{% endsnapshot %}
